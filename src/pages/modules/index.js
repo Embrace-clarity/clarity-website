b2b03383ca85584caf7cd062c15022ccdc256764
dimoFeeld <dimo@feeld.co>
2019-03-16 12:51:41 +0000

    Change webp to png

--- src/pages/modules/index.js
+++ src/pages/modules/index.js
@@ -1,18 +1,18 @@
-import React from "react"
-import Container from "../../components/container"
-import InnerContainer from "../../components/innerContainer"
-import styled from "styled-components"
-import Column from "../../components/column"
-import Heading from "../../components/heading"
-import ColumnsContainer from "../../components/columnsContainer"
-import Link from "../../components/link"
-import modules from "../../../static/modules.webp"
+import React from "react";
+import Container from "../../components/container";
+import InnerContainer from "../../components/innerContainer";
+import styled from "styled-components";
+import Column from "../../components/column";
+import Heading from "../../components/heading";
+import ColumnsContainer from "../../components/columnsContainer";
+import Link from "../../components/link";
+import modules from "../../../static/modules.png";
 
 const HeadingsContainer = styled.div`
   max-width: 700px;
-`
+`;
 
-const FooterContainer = styled.div``
+const FooterContainer = styled.div``;
 
 export default () => (
   <Container>
@@ -23,33 +23,33 @@ export default () => (
           Designed for the future.
         </Heading>
       </HeadingsContainer>
-      <img src={modules} alt="Modules Preview" />
+      <img src={modules} alt='Modules Preview' />
       <ColumnsContainer my={5}>
         <Column
-          title="Open Source"
-          subtitle="Future is transparent and everyone should be able to contribute to it."
-          subtitleColor="lightblack"
+          title='Open Source'
+          subtitle='Future is transparent and everyone should be able to contribute to it.'
+          subtitleColor='lightblack'
         />
         <Column
-          title="Modular design"
-          subtitle="Modularity will enable a more sustainable and ethical hardware manufacturing."
-          subtitleColor="lightblack"
+          title='Modular design'
+          subtitle='Modularity will enable a more sustainable and ethical hardware manufacturing.'
+          subtitleColor='lightblack'
         />
         <Column
-          title="Built by the community"
-          subtitle="Clarity is a non-profit project designed to support free flow of information."
-          subtitleColor="lightblack"
+          title='Built by the community'
+          subtitle='Clarity is a non-profit project designed to support free flow of information.'
+          subtitleColor='lightblack'
         />
       </ColumnsContainer>
       <FooterContainer>
         <Link
-          color="black"
-          href="https://spectrum.chat/embrace-clarity/general/how-to-contribute-design-code-ideas~d1a95605-b63a-497e-9cf7-416323953e77"
-          target="_blank"
+          color='black'
+          href='https://spectrum.chat/embrace-clarity/general/how-to-contribute-design-code-ideas~d1a95605-b63a-497e-9cf7-416323953e77'
+          target='_blank'
         >
           Contribute
         </Link>{" "}
       </FooterContainer>
     </InnerContainer>
   </Container>
-)
+);
