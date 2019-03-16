7f6fcf0e0c900ff584f4579c48a2b5e32ebb6154
dimoFeeld <dimo@feeld.co>
2019-03-16 12:18:33 +0000

    Added padding right on the column

--- src/components/column/index.js
+++ src/components/column/index.js
@@ -1,15 +1,20 @@
-import React from "react"
-import Container from "../../components/container"
-import Text from "../../components/text"
+import React from "react";
+import Text from "../../components/text";
+import styled from "styled-components";
+
+const ColumnContainer = styled.div`
+  padding-right: 16px;
+  flex: 1;
+`;
 
 function Column(props) {
   return (
-    <Container>
+    <ColumnContainer>
       <Text color={props.titleColor} mb={0}>
         {props.title}
       </Text>
       <Text color={props.subtitleColor}>{props.subtitle}</Text>
-    </Container>
-  )
+    </ColumnContainer>
+  );
 }
-export default Column
+export default Column;
