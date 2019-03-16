103eee096556a03ac93699db41a1a97b0e7f9bdc
dimoFeeld <dimo@feeld.co>
2019-03-16 12:51:18 +0000

    Webp issues with Safari

--- src/pages/os/index.js
+++ src/pages/os/index.js
@@ -1,58 +1,58 @@
-import React from "react"
-import Container from "../../components/container"
-import InnerContainer from "../../components/innerContainer"
-import styled from "styled-components"
-import Column from "../../components/column"
-import Heading from "../../components/heading"
-import ColumnsContainer from "../../components/columnsContainer"
-import osImage from "../../../static/os.webp"
-import Link from "../../components/link"
+import React from "react";
+import Container from "../../components/container";
+import InnerContainer from "../../components/innerContainer";
+import styled from "styled-components";
+import Column from "../../components/column";
+import Heading from "../../components/heading";
+import ColumnsContainer from "../../components/columnsContainer";
+import osImage from "../../../static/os.png";
+import Link from "../../components/link";
 
 const HeadingsContainer = styled.div`
   max-width: 700px;
-`
+`;
 
-const FooterContainer = styled.div``
+const FooterContainer = styled.div``;
 
 export default () => (
-  <Container bg="black">
+  <Container bg='black'>
     <InnerContainer>
       <HeadingsContainer>
-        <Heading mb={6} color="white">
+        <Heading mb={6} color='white'>
           Inspired by present time. <br />
           Designed for the future.
         </Heading>
       </HeadingsContainer>
-      <img src={osImage} alt="AOS Preview" />
+      <img src={osImage} alt='AOS Preview' />
       <ColumnsContainer my={5}>
         <Column
-          title="Decentralised"
-          subtitle="Native support for all major decentralised networks like Ethereum, IPFS, Dat and others."
-          subtitleColor="lightwhite"
-          titleColor="white"
+          title='Decentralised'
+          subtitle='Native support for all major decentralised networks like Ethereum, IPFS, Dat and others.'
+          subtitleColor='lightwhite'
+          titleColor='white'
         />
         <Column
-          title="Modules & Layers"
-          subtitle="Modules are going to replace standalone apps by allowing developers to add extra features on top of core Layers of the OS (Transportation, Communication, Payments, etc.)"
-          subtitleColor="lightwhite"
-          titleColor="white"
+          title='Modules & Layers'
+          subtitle='Modules are going to replace standalone apps by allowing developers to add extra features on top of core Layers of the OS (Transportation, Communication, Payments, etc.)'
+          subtitleColor='lightwhite'
+          titleColor='white'
         />
         <Column
-          title="Typographic UI"
-          subtitle="The minimalist approach to the UI will enable developers to create modules without the need of designers. "
-          subtitleColor="lightwhite"
-          titleColor="white"
+          title='Typographic UI'
+          subtitle='The minimalist approach to the UI will enable developers to create modules without the need of designers. '
+          subtitleColor='lightwhite'
+          titleColor='white'
         />
       </ColumnsContainer>
       <FooterContainer>
         <Link
-          color="white"
-          href="https://spectrum.chat/embrace-clarity/general/how-to-contribute-design-code-ideas~d1a95605-b63a-497e-9cf7-416323953e77"
-          target="_blank"
+          color='white'
+          href='https://spectrum.chat/embrace-clarity/general/how-to-contribute-design-code-ideas~d1a95605-b63a-497e-9cf7-416323953e77'
+          target='_blank'
         >
           Contribute
         </Link>
       </FooterContainer>
     </InnerContainer>
   </Container>
-)
+);
