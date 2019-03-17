19190cd175dd67792438571efcc63d6cbf0c6d64
dimoFeeld <dimo@feeld.co>
2019-03-17 20:14:41 +0000

    Quick layout fixes

--- src/components/heading/index.js
+++ src/components/heading/index.js
@@ -1,15 +1,9 @@
 import styled from "styled-components";
-import { color, space } from "styled-system";
+import { color, space, fontSize } from "styled-system";
 import { device } from "../../utils/device";
 
 const Heading = styled.h1`
-  @media ${device.mobileS} {
-    font-size: 32px;
-  }
-
-  @media ${device.tablet} {
-    font-size: 42px;
-  }
+  ${fontSize}
   ${color}
   ${space}
 `;
